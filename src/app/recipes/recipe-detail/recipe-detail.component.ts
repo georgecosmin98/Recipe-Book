b3983de5dc44b65e0125fb9c62d8ec03bd2a6b4d
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2022-01-30 16:19:41 +0200

    Building and Using Dropdown Directive

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,10 +9,8 @@ import { Recipe } from '../recipe.model';
 export class RecipeDetailComponent implements OnInit {
 
   @Input() recipe: Recipe;
-
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
