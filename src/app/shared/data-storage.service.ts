d07b32593fcbd4b3070f0c303503354667f9d53b
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2022-02-06 14:57:12 +0200

    Transforming Response Data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map } from "rxjs";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
+import { Ingredient } from "./ingredient.model";
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -16,8 +18,18 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.http.get<Recipe[]>('https://recipe-book-5eb22-default-rtdb.firebaseio.com/recipes.json').subscribe(recipes=> {
-            this.recipeService.setRecipes(recipes);
-        })
+        const emptyIngredientList: Ingredient[] = [];
+        this.http.get<Recipe[]>('https://recipe-book-5eb22-default-rtdb.firebaseio.com/recipes.json')
+            .pipe(map(recipes => {
+                return recipes.map(recipe => {
+                    return {
+                        ...recipe,
+                        ingredients: recipe.ingredients ? recipe.ingredients : emptyIngredientList
+                    };
+                })
+            }))
+            .subscribe(recipes => {
+                this.recipeService.setRecipes(recipes);
+            })
     }
 }
