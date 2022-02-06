158f9f8208b84e957e0279ff711cfe3ceba7e943
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2022-02-06 16:43:21 +0200

    Fixing bug with the resolver

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map } from "rxjs";
+import { map, tap } from "rxjs";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
-import { Ingredient } from "./ingredient.model";
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -18,18 +17,18 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        const emptyIngredientList: Ingredient[] = [];
-        this.http.get<Recipe[]>('https://recipe-book-5eb22-default-rtdb.firebaseio.com/recipes.json')
+        return this.http.get<Recipe[]>('https://recipe-book-5eb22-default-rtdb.firebaseio.com/recipes.json')
             .pipe(map(recipes => {
                 return recipes.map(recipe => {
                     return {
                         ...recipe,
-                        ingredients: recipe.ingredients ? recipe.ingredients : emptyIngredientList
+                        ingredients: recipe.ingredients ? recipe.ingredients : []
                     };
                 })
-            }))
-            .subscribe(recipes => {
-                this.recipeService.setRecipes(recipes);
-            })
+            }),
+                tap(recipes => {
+                    this.recipeService.setRecipes(recipes);
+                })
+            );
     }
 }
