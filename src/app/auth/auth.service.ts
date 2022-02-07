ff07c5586be1b3fe0e482c080ca824ddfc110ed5
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2022-02-07 13:26:11 +0200

    Creating & Storing the User Data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { catchError} from "rxjs/operators";
-import { throwError } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+import { Subject, throwError } from "rxjs";
 export interface AuthResponseData {
     kind: string;
     idToken: string;
@@ -13,9 +13,13 @@ export interface AuthResponseData {
 }
 
 import { FIREBASE_API_KEY } from '../../environment'
+import { UserModel } from "./user.model";
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+
+    user = new Subject<UserModel>();
+
     constructor(private http: HttpClient) { }
 
     signup(email: string, password: string) {
@@ -23,25 +27,41 @@ export class AuthService {
             email: email,
             password: password,
             returnSecureToken: true
-        }).pipe(catchError(errorRes => {
-            let errorMessage = 'An unknown error occurred!'
-            if (!errorRes.error || !errorRes.error.error) {
-                return throwError(() => errorMessage);
-            }
-            switch (errorRes.error.error.message) {
-                case 'EMAIL_EXISTS':
-                    errorMessage = 'This email already exists'
-            }
-            console.log(errorMessage)
-            return throwError(() => errorMessage)
-        }))
+        }).pipe(catchError(this.handleError), tap(resData => this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn)))
     }
 
-    login(email: string, password: string){
-        return this.http.post<AuthResponseData>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_API_KEY}`,{
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_API_KEY}`, {
             email: email,
             password: password,
             returnSecureToken: true
-        })
+        }).pipe(catchError(this.handleError), tap(resData => this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn)))
+    }
+
+    private handleError(errorRes: HttpErrorResponse) {
+        let errorMessage = 'An unknown error occurred!'
+        if (!errorRes.error || !errorRes.error.error) {
+            return throwError(() => errorMessage);
+        }
+        switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+                errorMessage = 'This email already exists!'
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = 'This email does not exist!'
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = 'This password is not correct!'
+                break;
+
+        }
+        console.log(errorMessage)
+        return throwError(() => errorMessage)
+    }
+
+    private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+        const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+        const user = new UserModel(email, userId, token, expirationDate)
+        this.user.next(user);
     }
 }
